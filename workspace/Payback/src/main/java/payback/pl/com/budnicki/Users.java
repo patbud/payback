bd4be61dac35946f376cc83dfcfeb7bad97b8fb4
patbud <p@budnicki.com.pl>
2016-10-10 14:19:22 +0200

    initial

---
+++ workspace/Payback/src/main/java/payback/pl/com/budnicki/Users.java
@@ -0,0 +1,33 @@
+package payback.pl.com.budnicki;
+
+public class Users {
+	int idUsers;
+	String name;
+	String surname;
+	String city;
+	
+	public int getIdUsers() {
+		return idUsers;
+	}
+	public void setIdUsers(int idUsers) {
+		this.idUsers = idUsers;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+}
